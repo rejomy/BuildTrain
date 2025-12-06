0c386423c7f318066ce48649e230afcfab4e0fea
Rejomy <iyaargg@gmail.com>
2025-12-07 00:00:50 +0500

    Do not allow drop items

--- src/main/java/me/rejomy/buildtrain/listener/ItemListener.java
+++ src/main/java/me/rejomy/buildtrain/listener/ItemListener.java
@@ -2,8 +2,6 @@ package me.rejomy.buildtrain.listener;
 
 import me.rejomy.buildtrain.Main;
 import me.rejomy.buildtrain.island.Island;
-import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,9 +12,8 @@ public class ItemListener implements Listener {
     public void onDrop(PlayerDropItemEvent event) {
         Player player = event.getPlayer();
         Island island = Main.getInstance().USERS.get(player).island;
-        Item item = event.getItemDrop();
 
-        if (island != null && item.getItemStack().getType() == Material.REDSTONE_COMPARATOR) {
+        if (island != null) {
             event.setCancelled(true);
         }
     }
